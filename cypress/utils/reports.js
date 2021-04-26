6b206b937ec7fff4fc3f762f030a4df07d81311d
Przemek Podsiadlik <ppodsiadlik@future-processing.com>
2021-04-26 14:02:05 +0200

    Adds test reporter

---
+++ cypress/utils/reports.js
@@ -0,0 +1,63 @@
+const cypress = require("cypress");
+const marge = require("mochawesome-report-generator");
+const { merge } = require("mochawesome-merge");
+const fs = require("fs-extra");
+const argv = require("minimist")(process.argv.slice(2));
+
+const cypressReportFolder = "cypress/reports";
+const cypressTemporaryReportFolder = `${cypressReportFolder}/temporary`;
+const tmpReportFolderPath = `${process.cwd()}/${cypressTemporaryReportFolder}`;
+
+function deleteReport() {
+    return fs.remove(tmpReportFolderPath).catch(err => console.log(err));
+}
+
+const mergeOptions = {
+    files: [`./${cypressTemporaryReportFolder}/*.json`]
+};
+
+const generatorOptions = {
+    reportFilename: `${new Date().toJSON()}`,
+    reportDir: `./${cypressReportFolder}`
+};
+
+async function generateReport() {
+    const report = await merge(mergeOptions);
+    await marge.create(report, generatorOptions);
+}
+
+function cypressSettings(browser, headless) {
+    if (browser === undefined) {
+        argv.browser = "chrome";
+    }
+    if (headless === undefined) {
+        argv.headless = "false";
+    }
+    const isHeadless = (argv.headless = argv.headless === "true");
+
+    return {
+        browser: argv.browser,
+        headless: isHeadless,
+        quiet: false
+        // uncomment below if you only want to run tests from one of the files
+        // spec: ".path-to-test"
+    };
+}
+
+async function runTests(argv) {
+    await deleteReport();
+    try {
+        const results = await cypress.run(cypressSettings(argv.browser, argv.headless));
+        await generateReport();
+        if (results.totalFailed) {
+            process.exit(1);
+        }
+        process.exit(0);
+    } catch (error) {
+        await generateReport();
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+runTests(argv);
