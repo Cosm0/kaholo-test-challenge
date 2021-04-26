2008f1ab68b227480ccbc2448d37dcae1ac4ba07
Przemek Podsiadlik <ppodsiadlik@future-processing.com>
2021-04-26 17:50:03 +0200

    Skip local storage tests

--- cypress/tests/localStorage/localStorage.spec.js
+++ cypress/tests/localStorage/localStorage.spec.js
@@ -2,7 +2,7 @@ import { homePageActions } from "../../pages/home/home.actions";
 import { settingsPageActions } from "../../pages/settings/settings.actions";
 import { localStorageTestActions } from "./localStorage.testActions";
 
-
+// Below are not a legit tests hence I skip them. Just added them to show Cypress capabilities to operate on local storage.
 context("Test challenge website local storage", () => {
     const expectedLogLevel = 'INFO';
 
@@ -10,7 +10,7 @@ context("Test challenge website local storage", () => {
         { page: 'home', navigation: homePageActions.visit },
         { page: 'settings', navigation: settingsPageActions.visit }
     ].forEach((testCase) =>
-        it(`Should be set log level to ${expectedLogLevel} after navigation to ${testCase.page}`, () => {
+        it.skip(`Should be set log level to ${expectedLogLevel} after navigation to ${testCase.page}`, () => {
             // When
             testCase.navigation();
             // Then
