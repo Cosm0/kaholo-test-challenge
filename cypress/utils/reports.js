bfbd772c9342bb6af5b76b197e3f7d304ba51683
Przemek Podsiadlik <ppodsiadlik@future-processing.com>
2021-04-26 23:20:52 +0200

    Dockerization

--- cypress/utils/reports.js
+++ cypress/utils/reports.js
@@ -27,7 +27,6 @@ async function generateReport() {
 }
 
 function cypressSettings(browser, headless) {
-    console.log(headless)
     if (browser === undefined) {
         argv.browser = "chrome";
     }
@@ -60,5 +59,5 @@ async function runTests(argv) {
         process.exit(1);
     }
 }
-console.log(argv.headless);
+
 runTests(argv);
